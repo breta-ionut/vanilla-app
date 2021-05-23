3f182cd9a77ea3087b6f15fbb2c7c0ecbeb6a33a
Ionut Breta <breta.ionut@gmail.com>
2021-05-23 22:56:02 +0300

    - enhanced no route exception

--- src/Core/Exception/NoRouteFoundException.php
+++ src/Core/Exception/NoRouteFoundException.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace App\Core\Exception;
 
-class NoRouteFoundException extends \DomainException
+use Symfony\Component\HttpFoundation\Response;
+
+class NoRouteFoundException extends \DomainException implements HttpExceptionInterface
 {
     public function __construct(string $path, string $method, int $code = 0, \Throwable $previous = null)
     {
         $message = \sprintf('No route found for path "%s" and method "%s".', $path, $method);
 
         parent::__construct($message, $code, $previous);
+    }
+
+    public function getStatus(): int
+    {
+        return Response::HTTP_NOT_FOUND;
     }
 }
