6ee36945e0aa9a26e230de504b8efe4280700778
Ionut Breta <breta.ionut@gmail.com>
2021-05-24 22:59:07 +0300

    - fixed parameter name

--- config/services.php
+++ config/services.php
@@ -7,7 +7,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 return function (ContainerConfigurator $configurator): void {
     $configurator->parameters()
         ->set('database.host', param('env(MYSQL_HOST)'))
-        ->set('database.name', param('env(MYSQL_DATABASE'))
+        ->set('database.name', param('env(MYSQL_DATABASE)'))
         ->set('database.user', param('env(MYSQL_USER)'))
         ->set('database.password', param('env(MYSQL_PASSWORD)'));
 };
