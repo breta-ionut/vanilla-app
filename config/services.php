5ac5d6b869315e0a113834d6b5a934889c6f0052
Ionut Breta <breta.ionut@gmail.com>
2021-05-24 22:56:59 +0300

    - configured database

--- config/services.php
+++ config/services.php
@@ -5,4 +5,9 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 return function (ContainerConfigurator $configurator): void {
+    $configurator->parameters()
+        ->set('database.host', param('env(MYSQL_HOST)'))
+        ->set('database.name', param('env(MYSQL_DATABASE'))
+        ->set('database.user', param('env(MYSQL_USER)'))
+        ->set('database.password', param('env(MYSQL_PASSWORD)'));
 };
