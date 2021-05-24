6d7299af87e0c00e451e67a472c09e9d90984c4b
Ionut Breta <breta.ionut@gmail.com>
2021-05-25 01:29:33 +0300

    - small change in entrypoint

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-$kernel = new Kernel((bool) ($_SERVER['APP_ENV'] ?? true));
+$kernel = new Kernel((bool) ($_SERVER['APP_DEBUG'] ?? false));
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
